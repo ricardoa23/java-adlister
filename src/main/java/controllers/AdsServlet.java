0d4a513ed7e77771d85e7330cb06d270ae339587
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-03-14 15:06:38 -0500

    added some note after instructor walk through

--- src/main/java/controllers/AdsServlet.java
+++ src/main/java/controllers/AdsServlet.java
@@ -4,14 +4,21 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+
+import dao.Ads;
 import dao.DaoFactory;
+import dao.ListAdsDao;
 
 
 @WebServlet(name = "controllers.AdsServlet", value = "/ads")
 public class AdsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+//  Instructors methods makes it better for performance and for future proofing if code needs to be refactored
+        Ads adsDao = DaoFactory.getAdsDao();
+
+            request.setAttribute("ads", adsDao.all());
+//        request.setAttribute("ads", DaoFactory.getAdsDao().all()); this is the method I used no separation of concerns
         request.getRequestDispatcher("/ads/index.jsp").forward(request,response);
     }
 }
